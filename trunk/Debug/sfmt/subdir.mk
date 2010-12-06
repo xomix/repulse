ed631a839a24d8fedc5c5f045cdec1b88c736d80
jrodrigog <jrodrigog@999c8263-88bf-9d15-4b50-154109809ae7>
2010-12-06 15:45:39 +0000

    Initial commit 0.0.1

--- trunk/Debug/sfmt/subdir.mk
+++ trunk/Debug/sfmt/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 sfmt/%.o: ../sfmt/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/include -I/usr/lib/gcc/i486-slackware-linux/4.4.4/include -I/usr/include/c++/4.4.4 -O3 -g3 -pedantic -pedantic-errors -Wall -Werror -c -fmessage-length=0 -msse2 -fno-strict-aliasing -march=i486 -mtune=i686 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -O3 -g3 -pedantic -pedantic-errors -Wall -Werror -c -fmessage-length=0 -msse2 -fno-strict-aliasing -march=i486 -mtune=i686 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
