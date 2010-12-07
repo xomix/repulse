a863142faccedd5762e13c248f4f796a19888fe4
jrodrigog <jrodrigog@999c8263-88bf-9d15-4b50-154109809ae7>
2010-12-07 16:15:24 +0000

    0.0.1

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /home/jrodrigo/workspace/repulse/soundtouch/source/SoundTouch/.libs/libSoundTouch.a
-
-LIBS := -ljack -lncurses -lasound -lsndfile -lsamplerate
-
