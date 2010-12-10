4d3900057a166fa960461c00e60ea476739fe908
jrodrigog <jrodrigog@999c8263-88bf-9d15-4b50-154109809ae7>
2010-12-10 17:59:35 +0000

    0.0.1

---
+++ trunk/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := ../soundtouch/source/SoundTouch/.libs/libSoundTouch.a
+
+LIBS := -lasound -lsamplerate -lcurses -lsndfile -ljack
+
---
+++ trunk/Debug/tinyxml/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../tinyxml/tinystr.cpp \
+../tinyxml/tinyxml.cpp \
+../tinyxml/tinyxmlerror.cpp \
+../tinyxml/tinyxmlparser.cpp 
+
+OBJS += \
+./tinyxml/tinystr.o \
+./tinyxml/tinyxml.o \
+./tinyxml/tinyxmlerror.o \
+./tinyxml/tinyxmlparser.o 
+
+CPP_DEPS += \
+./tinyxml/tinystr.d \
+./tinyxml/tinyxml.d \
+./tinyxml/tinyxmlerror.d \
+./tinyxml/tinyxmlparser.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+tinyxml/%.o: ../tinyxml/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"../soundtouch/source/SoundTouch" -I"../soundtouch/include" -O3 -g3 -pedantic -pedantic-errors -Wall -Werror -c -fmessage-length=0 -pedantic -msse2 -fno-strict-aliasing -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
