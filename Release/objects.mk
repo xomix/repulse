8fa4b6cbbf56a4e4aed02926c75faf2aafe5afcd
jrodrigog <jrodrigog@999c8263-88bf-9d15-4b50-154109809ae7>
2010-12-20 22:47:59 +0000

    0.0.1

--- Release/objects.mk
+++ Release/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS := ../soundtouch/source/SoundTouch/.libs/libSoundTouch.a
 
-LIBS := -ljack -lncurses -lasound -lsndfile -lsamplerate
+LIBS := -lasound -ljack -lsndfile -lsamplerate -lcurses
 
